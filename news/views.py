cda093c737787e0ca0c0b6a8481bc4c0f255a0e1
AMuh2020 <leasu.muhmeen@gmail.com>
2023-08-28 06:30:00 +0300

    some progress+ new files

--- news/views.py
+++ news/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import News
 # Create your views here.
+def news(request):
+    news = News.objects.all()
+    return render(request, 'news/news.html',{'news':news})
